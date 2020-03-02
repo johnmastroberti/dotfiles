cf786050b260c5da5eb39216b70bf05a4cc9bc54
John Mastroberti <jackmastroberti@aol.com>
2020-03-02 14:11:14 -0500

    dwm config folder

---
+++ .config/dwm/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+compton &
+setbg &
+# Shuffle wallpapers
+while true; do
+  setbg "/home/john/Pictures/backgrounds/$(ls /home/john/Pictures/backgrounds/ | shuf | head -n 1)"
+  sleep 10m
+done &
+dwmblocks &
